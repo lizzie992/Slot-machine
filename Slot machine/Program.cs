1df6f1d9166b7c76736ec61ef4c8570b7fcee1b8
lizzie992 <gulyaskata99@gmail.com>
2024-08-16 20:45:49 +0200

    added the element for couting the loops (i)

--- Slot machine/Program.cs
+++ Slot machine/Program.cs
@@ -23,7 +23,8 @@ namespace Slot_machine
             const string INSTRUCTION = $"Please select the game mode below!\r\nYou can choose between: \r\nC={C} \r\nH={H} \r\nV={V} \r\nD={D}";
 
             //creating a 2D array with the desired number of items:
-            int[,] table = new int[SIZE, SIZE];
+            //int[,] table = new int[SIZE, SIZE];
+            int[,] table = { { 1, 1, 1 }, { 1, 2, 3 }, { 1, 2, 3 } };
 
             //defining the random number
             Random random = new Random();
@@ -33,17 +34,17 @@ namespace Slot_machine
             int c = 0;
             int r = 0;
 
-            //adding random elements to the 2D array:
-            for (r = 0; r < SIZE; r++)
-            {
-                for (c = 0; c < SIZE; c++)
-                {
-                    //lets define a random number between 1 and 9
-                    int randomNumber = random.Next(MIN_NUMBER, MAX_NUMBER);
-                    //and now add it:
-                    table[r, c] = randomNumber;
-                }
-            }
+            ////adding random elements to the 2D array:
+            //for (r = 0; r < SIZE; r++)
+            //{
+            //    for (c = 0; c < SIZE; c++)
+            //    {
+            //        //lets define a random number between 1 and 9
+            //        int randomNumber = random.Next(MIN_NUMBER, MAX_NUMBER);
+            //        //and now add it:
+            //        table[r, c] = randomNumber;
+            //    }
+            //}
 
             Console.WriteLine(INSTRUCTION);
             string answer = Console.ReadLine().ToUpper();
@@ -51,7 +52,7 @@ namespace Slot_machine
             //Lets print our grid but in a nice way, every row in a different line:
             for (r = 0; r < SIZE; r++)
             {
-                for(c = 0; c < SIZE; c++)
+                for (c = 0; c < SIZE; c++)
                 {
                     Console.Write(table[r, c]);
                 }
@@ -59,14 +60,21 @@ namespace Slot_machine
             }
 
             if (answer == "H")  // horizontal lines:
-            { 
+            {
+                int i = 0;
                 for (r = 0; r < SIZE; r++)
                 {
                     for (c = 0; c < SIZE - 1; c++)
                     {
-                        if (table[r, c] == table[r, c + 1]) //if there is a match somewhere in the row:
+                        if (table[r, c] != table[r, c + 1]) //as soon as we have a mismatch, we break the loop and move to the next row
                         {
-                            if (c == SIZE - 2) //if we reach the last iteration of the loop and we are still matching, it means that we have a whole line of matches. I write the winning line here as well so that the player can see:
+                            Console.WriteLine($"The {r + 1}. horizontal row did not win!");
+                            break;
+                        }
+                        else if (table[r, c] == table[r, c + 1]) //if there is a match somewhere in the row:
+                        {
+                            i++;
+                            if (i == SIZE-1)
                             {
                                 Console.Write($"You win! There is a match in the {r + 1}. horizontal row: ");
                                 for (c = 0; c < SIZE; c++)
@@ -74,17 +82,11 @@ namespace Slot_machine
                                     Console.Write(table[r, c]);
                                 }
                                 Console.WriteLine($"\r");
-                                break;
                             }
-                            else //if it is not the last matching pair, then continue in the next row
+                            else 
                             {
                                 continue;
                             }
-                        }
-                        else if (table[r, c] != table[r, c + 1]) //as soon as we have a mismatch, we break the loop and move to the next row
-                        {
-                            Console.WriteLine($"The {r+1}. horizontal row did not win!");
-                            break;
                         }
                     }
                 }
@@ -92,13 +94,20 @@ namespace Slot_machine
 
             if (answer == "V")  // vertical lines:
             {
+                int i = 0;
                 for (c = 0; c < SIZE; c++)
                 {
                     for (r = 0; r < SIZE - 1; r++)
                     {
-                        if (table[r, c] == table[r+1, c]) //if there is a match somewhere in the column:
+                        if (table[r, c] != table[r + 1, c]) //as soon as we have a mismatch, we break the loop and move to the next row
                         {
-                            if (r == SIZE - 2) //if we reach the last iteration of the loop and we are still matching, it means that we have a whole line of matches. I write the winning line here as well so that the player can see:
+                            Console.WriteLine($"The {c + 1}. vertical line did not win!");
+                            break;
+                        }
+                        else if (table[r, c] == table[r + 1, c]) //if there is a match somewhere in the column:
+                        {
+                            i++;
+                            if (i == SIZE - 1)
                             {
                                 Console.Write($"You win! There is a match in the {c + 1}. vertical line: ");
                                 for (r = 0; r < SIZE; r++)
@@ -106,17 +115,11 @@ namespace Slot_machine
                                     Console.Write(table[r, c]);
                                 }
                                 Console.WriteLine($"\r");
-                                break;
                             }
-                            else //if it is not the last matching pair, then continue in the next row
+                            else
                             {
                                 continue;
                             }
-                        }
-                        else if (table[r, c] != table[r+1, c]) //as soon as we have a mismatch, we break the loop and move to the next row
-                        {
-                            Console.WriteLine($"The {c + 1}. vertical line did not win!");
-                            break;
                         }
                     }
                 }
